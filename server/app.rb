048036654f08eb0de086276a469a41be1841b6ce
typewriter <takuyaxp@gmail.com>
2020-02-02 17:24:27 +0900

    Implement server application including delay estimation

---
+++ server/app.rb
@@ -0,0 +1,125 @@
+#!/usr/bin/env ruby
+
+require "sinatra"
+require "sinatra/json"
+
+require "net/http"
+require "uri"
+require "tempfile"
+require "fileutils"
+require "time"
+
+require "rufus-scheduler"
+
+# scheduler = Rufus::Scheduler.new
+# scheduler.every '3s' do
+#   puts 'Hello!'
+# end
+
+DATA_DIR = File.dirname(__FILE__)
+API_ENDPOINT = "https://api.tokyometroapp.jp/api/v2/datapoints"
+CONSUMER_KEY = ENV['CONSUMER_KEY']
+
+def railways
+  JSON.parse(File.read(File.join(DATA_DIR, "railways.json")))
+end
+
+def timetables(id)
+  filename = id.gsub(/[\\\/:*?"<>|]/, "-")
+  JSON.parse(File.read(File.join(DATA_DIR, "timetables.#{filename}.json")))
+end
+
+def trains(id)
+  filename = id.gsub(/[\\\/:*?"<>|]/, "-")
+  filepath = File.join(DATA_DIR, "trains.#{filename}.json")
+
+  # 次のケースを考慮する
+  # - 未取得の場合
+  # - パース出来ないデータの場合
+  # - 期限切れの場合
+  is_need_to_update = false
+  is_need_to_update ||= !File.exist?(filepath)
+
+  if File.exist?(filepath)
+    begin
+      json = JSON.parse(File.read(filepath))
+      is_need_to_update ||= !json.dig(0, "dct:valid") || Time.now > Time.parse(json.first["dct:valid"]) + 5
+    rescue
+      is_need_to_update = true
+    end
+  end
+
+  if is_need_to_update
+    puts "refresh"
+    body = Net::HTTP.get(URI.parse("#{API_ENDPOINT}?rdf:type=odpt:Train&acl:consumerKey=#{CONSUMER_KEY}"))
+    json = JSON.parse(body)
+
+    railways.each { |railway|
+      railway_filename = railway["railway"]["id"].gsub(/[\\\/:*?"<>|]/, "-")
+      railway_json = json.select { |train| train["odpt:railway"] == railway["railway"]["id"] }
+
+      tempfile = Tempfile.create
+      tempfile.write JSON.generate(railway_json)
+      tempfile.close
+      FileUtils.mv(tempfile.path, File.join(DATA_DIR, "trains.#{railway_filename}.json"))
+    }
+  end
+
+  begin
+    json = JSON.parse(File.read(filepath))
+  rescue
+    []
+  end
+end
+
+get '/railways' do
+  json railways
+end
+
+get '/trains/:id' do
+  if !railways.any? { |railway| railway.dig("railway", "id") == params[:id] }
+    halt 400
+  end
+
+  day_of = "saturdaysHolidays"
+  timetables = timetables(params[:id])
+  trains = trains(params[:id])
+
+  out_trains = trains.map { |train|
+    {
+      id: train["owl:sameAs"],
+      trainType: train["odpt:trainType"],
+      starting: train["odpt:startingStation"],
+      terminal: train["odpt:terminalStation"],
+      direction: train["odpt:railDirection"],
+      current: train["odpt:fromStation"],
+      next: train["odpt:toStation"],
+      # timetable: timetables[day_of][train["owl:sameAs"]]
+    }.tap { |out_train|
+      if timetable = timetables.dig(day_of, train["owl:sameAs"])
+        # 発車済の場合は次駅時間、未発車の場合は当駅時間で
+        current_time = train["dc:date"]
+        planned_time = timetable.dig("stops", out_train[:next] || out_train[:current])
+        if !current_time || !planned_time
+          out_train[:delay] = nil
+          break out_train
+        end
+
+        current_time = Time.parse(current_time)
+        planned_time = Time.parse(planned_time)
+
+        if current_time - planned_time > 60
+          out_train[:delay] = ((current_time - planned_time) / 60).to_i
+        else
+          out_train[:delay] = 0
+        end
+      end
+    }
+  }
+
+  json({ date: trains.dig(0, "dc:date"), trains: out_trains })
+end
+
+get '/' do
+  "Hello, world!"
+end
---
+++ server/get_railway.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+
+require "net/http"
+require "uri"
+require "json"
+require "tempfile"
+require "fileutils"
+
+data_dir = File.dirname(__FILE__)
+
+API_ENDPOINT = "https://api.tokyometroapp.jp/api/v2/datapoints"
+CONSUMER_KEY = ENV['CONSUMER_KEY']
+
+
+## 路線・駅情報の集約
+json = Net::HTTP.get(URI.parse("#{API_ENDPOINT}?rdf:type=odpt:Station&acl:consumerKey=#{CONSUMER_KEY}"))
+stations = JSON.parse(json)
+station_map = stations.map { |station|
+  [station["owl:sameAs"], { id: station["owl:sameAs"], name: station["dc:title"], stationCode: station["odpt:stationCode"] }]
+}.to_h
+
+sleep 10
+
+json = Net::HTTP.get(URI.parse("#{API_ENDPOINT}?rdf:type=odpt:Railway&acl:consumerKey=#{CONSUMER_KEY}"))
+railways = JSON.parse(json)
+railways = railways.map { |railway|
+  {
+    railway: { id: railway["owl:sameAs"], name: railway["dc:title"] },
+    stations: railway["odpt:stationOrder"].map { |station_order|
+      station_map[station_order["odpt:station"]]
+    }
+  }
+}
+
+tempfile = Tempfile.create
+tempfile.write JSON.generate(railways)
+tempfile.close
+FileUtils.mv(tempfile.path, File.join(data_dir, "railways.json"))
+
+railways = JSON.parse(File.read("./railways.json"))
+
+## 時刻表の取得
+railways.each { |railway|
+  sleep 10
+  json = Net::HTTP.get(URI.parse("#{API_ENDPOINT}?rdf:type=odpt:TrainTimetable&acl:consumerKey=#{CONSUMER_KEY}&odpt:railway=#{railway["railway"]["id"]}"))
+  timetables = JSON.parse(json)
+
+  output_timetables = { weekdays: {}, saturdaysHolidays: {} }
+  timetables.each { |timetable|
+    key = (timetable["owl:sameAs"] =~ /SaturdaysHolidays/) ? :saturdaysHolidays : :weekdays
+    output_timetables[key][timetable["odpt:train"]] = {
+      type: timetable["odpt:trainType"],
+      starting: timetable["odpt:startingStation"],
+      terminal: timetable["odpt:terminalStation"],
+      direction: timetable["odpt:railDirection"],
+      stops: timetable["odpt:#{key == :weekdays ? "weekdays": "holidays"}"].map { |stop|
+        [stop["odpt:departureStation"] || stop["odpt:arrivalStation"], stop["odpt:departureTime"] || stop["odpt:arrivalTime"]]
+      }.to_h
+    }
+  }
+
+  tempfile = Tempfile.create
+  tempfile.write JSON.generate(output_timetables)
+  tempfile.close
+
+  filename = railway["railway"]["id"].gsub(/[\\\/:*?"<>|]/, "-")
+  FileUtils.mv(tempfile.path, File.join(data_dir, "timetables.#{filename}.json"))
+}
+
